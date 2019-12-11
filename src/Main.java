bb80f1008c97f9d16718372e6080eeaea3f8270b
Maciej <maciejkozub99@gmail.com>
2019-12-12 00:10:17 +0100

    little markups

--- src/Main.java
+++ src/Main.java
@@ -13,14 +13,5 @@ public class Main {
         menuFrame.add(new Settings(initialDefaultValues));
         menuFrame.setVisible(true);
 
-       /* WorldMap map = new WorldMap(200, 100,
-                100, 30, 10, 10, 20, 100,
-                2000, 1, 1, 1);
-        for(int i=0; i<100; i++){
-            map.day();
-            System.out.println("");
-            System.out.println("");
-        }*/
-
     }
 }
